b07b6b04ffde83feb2482c3c705e939a61a2b003
OpenedClosed <opendoor200179@gmail.com>
2025-03-19 22:32:10 +0300

    TEST закомментировал бриф

--- backend/fastapi_web/chats/utils/knowledge_base.py
+++ backend/fastapi_web/chats/utils/knowledge_base.py
@@ -3,26 +3,26 @@
 from chats.db.mongo.schemas import BriefQuestion
 
 BRIEF_QUESTIONS = [
-    BriefQuestion(
-        question="Favorite season?",
-        question_translations={
-            "en": "Favorite season?",
-            "ru": "Любимое время года?"
-        }
-    ),
-    BriefQuestion(
-        question="Choice?",
-        question_translations={
-            "en": "Choice?",
-            "ru": "Выбор?"
-        },
-        question_type="choice",
-        expected_answers=["Var1", "Var2"],
-        expected_answers_translations={
-            "en": ["Var1", "Var2"],
-            "ru": ["Вар1", "Вар2"]
-        }
-    )
+    # BriefQuestion(
+    #     question="Favorite season?",
+    #     question_translations={
+    #         "en": "Favorite season?",
+    #         "ru": "Любимое время года?"
+    #     }
+    # ),
+    # BriefQuestion(
+    #     question="Choice?",
+    #     question_translations={
+    #         "en": "Choice?",
+    #         "ru": "Выбор?"
+    #     },
+    #     question_type="choice",
+    #     expected_answers=["Var1", "Var2"],
+    #     expected_answers_translations={
+    #         "en": ["Var1", "Var2"],
+    #         "ru": ["Вар1", "Вар2"]
+    #     }
+    # )
 ]
 
 KNOWLEDGE_BASE = {
