63b57af99679552b048631b3b136b9501d4dde33
OpenedClosed <opendoor200179@gmail.com>
2025-04-11 20:05:00 +0300

    TEST Скрыл сообщения в админке

--- backend/fastapi_web/chats/admin.py
+++ backend/fastapi_web/chats/admin.py
@@ -322,4 +322,4 @@ class ChatSessionAdmin(BaseAdmin):
 
 admin_registry.register("chat_sessions", ChatSessionAdmin(mongo_db))
 # admin_registry.register("clients", ClientInline(mongo_db))
-admin_registry.register("chat_messages", ChatMessageInline(mongo_db))
+# admin_registry.register("chat_messages", ChatMessageInline(mongo_db))
