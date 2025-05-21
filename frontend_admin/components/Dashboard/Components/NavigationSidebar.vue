b4c2e00a2eb5f3f70c366028927607a539cf3e07
SerjioSA <s.a.sander@yandex.ru>
2025-05-21 20:04:24 +0400

    feat: Add admin role check and fetch user data in NavigationSidebar component

--- frontend_admin/components/Dashboard/Components/NavigationSidebar.vue
+++ frontend_admin/components/Dashboard/Components/NavigationSidebar.vue
@@ -47,7 +47,7 @@
         </li>
 
         <!-- hard-coded Admin settings -->
-        <li v-if="currentPageName === 'admin'">
+        <li v-if="currentPageName === 'admin' && isAdmin">
           <h3 v-if="!isCollapsed" class="text-md uppercase mb-2 flex items-center px-2 font-semibold">
             <i class="mr-2 pi pi-calendar-plus"></i>
             {{t('NavigationSidebar.headers.settings')}}
@@ -115,7 +115,7 @@
             </li>
           </ul>
         </li>
-        <li v-if="currentPageName === 'admin'">
+        <li v-if="currentPageName === 'admin' && isAdmin">
           <h3 class="text-md uppercase flex justify-start text-ellipsis items-center mb-2 px-2">
             <i class="mr-2 flex pi pi-calendar-plus"></i>
             Settings
@@ -197,6 +197,45 @@ watch(
   () => props.navItems,
   (val) => console.log("navItems updated", val)
 );
+
+
+
+
+// 
+
+let isAdmin = ref(false);
+// Получаем данные из API через useAsyncData (Nuxt 3)
+const { data: headerData, error } = await useAsyncData("headerData", getHeaderData);
+
+if (headerData.value) {
+  // Destructure the two objects from headerData.value
+  const { responseDataMe } = headerData.value;
+  console.log("responseDataMe", responseDataMe);
+  if (responseDataMe?.role && (responseDataMe?.role === "admin" || responseDataMe?.role === "superadmin")) {
+    isAdmin.value = true;
+  }
+
+
+  // For example, if you want to log responseDataMe:
+  console.log("User data =", responseDataMe);
+} else if (error.value) {
+  console.error("Ошибка загрузки данных:", error.value);
+}
+
+
+// Функция для получения данных из API
+async function getHeaderData() {
+  let responseDataMe;
+  try {
+    const response_me = await useNuxtApp().$api.get(`/api/users/me`);
+    responseDataMe = response_me.data;
+  } catch (err) {
+    if (err.response) {
+      console.error("Ошибка API:", err.response.data);
+    }
+  }
+  return {  responseDataMe };
+}
 </script>
 
 <style scoped>
